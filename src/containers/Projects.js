1f408b556bf4b56ed29e9956f2a459130490d5c5
OccassionalDev <ericaschimmel01@gmail.com>
2020-11-04 23:54:14 -0500

    added containers for About and Projects

---
+++ src/containers/Projects.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class Projects extends Component {
+    render() {
+        return (
+            <div> 
+            </div>
+        )
+    }
+}
+
+export default Projects
